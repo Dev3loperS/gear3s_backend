a0357d60c38f0979f09dc727f345e42d0e7403d8
teddy1312 <107341701+teddy1312@users.noreply.github.com>
2023-03-25 00:25:09 +0900

    initial commit

---
+++ gear3s-project/src/main/java/cybersoft/java20/dev3lopers/gear3sproject/Gear3sProjectApplication.java
@@ -0,0 +1,13 @@
+package cybersoft.java20.dev3lopers.gear3sproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gear3sProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gear3sProjectApplication.class, args);
+	}
+
+}
---
+++ gear3s-project/src/test/java/cybersoft/java20/dev3lopers/gear3sproject/Gear3sProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cybersoft.java20.dev3lopers.gear3sproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gear3sProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
