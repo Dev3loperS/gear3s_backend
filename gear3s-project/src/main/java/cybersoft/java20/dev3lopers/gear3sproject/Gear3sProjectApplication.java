643405ab1bf872a1963923e5c23e11f731e8cf5e
truongdat-windows <truongdat1408@gmail.com>
2023-04-04 15:50:20 +0700

    start thymeleaf

--- gear3s-project/src/main/java/cybersoft/java20/dev3lopers/gear3sproject/Gear3sProjectApplication.java
+++ gear3s-project/src/main/java/cybersoft/java20/dev3lopers/gear3sproject/Gear3sProjectApplication.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import javax.crypto.SecretKey;
 
--- gear3s-project/src/main/java/cybersoft/java20/dev3lopers/gear3sproject/controller/admin/LoginThymeleafController.java
+++ gear3s-project/src/main/java/cybersoft/java20/dev3lopers/gear3sproject/controller/admin/LoginThymeleafController.java
@@ -1,2 +1,16 @@
-package cybersoft.java20.dev3lopers.gear3sproject.controller.admin;public class LoginThymeleafController {
+package cybersoft.java20.dev3lopers.gear3sproject.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class LoginThymeleafController {
+    @GetMapping("/")
+    public String login(Model model) {
+        model.addAttribute("message", "Dat");
+        return "admin-login";
+    }
 }
