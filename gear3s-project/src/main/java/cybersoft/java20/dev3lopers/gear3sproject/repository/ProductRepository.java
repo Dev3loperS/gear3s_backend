adfe796f30c91461f6640e7699ff31bc7e9fceb2
ThanhBear <leethanh24003@gmail.com>
2023-04-20 16:14:12 +0700

    fix conflict 3

--- gear3s-project/src/main/java/cybersoft/java20/dev3lopers/gear3sproject/repository/ProductRepository.java
+++ gear3s-project/src/main/java/cybersoft/java20/dev3lopers/gear3sproject/repository/ProductRepository.java
@@ -3,7 +3,6 @@ package cybersoft.java20.dev3lopers.gear3sproject.repository;
 import cybersoft.java20.dev3lopers.gear3sproject.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,14 +13,7 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     Product findById(int id);
 
-    //@Query(value = "select p from  product p WHERE p.name LIKE CONCAT('%',?1,'%' )")
-   //List<Product> findByName(String name);
 
-    //@Query(value = "select p from  product p WHERE p.discountPrice BETWEEN  ?1 AND ?2")
-   //List<Product> findByOriginPrice(int minPrice, int maxPrice);
-
-    //@Query(value = "select p from  product p WHERE p.discountPrice BETWEEN  ?1 AND ?2")
-   // List<Product> findByDiscountPrice(int minPrice, int maxPrice);
 
     void deleteById(int id);
 
