6a4ea6376cc11c1646e9f1294fc01b73ec607fe2
teddy1312 <tuanhnguyen1312@gmail.com>
2023-04-25 23:07:13 +0900

    fix register bug

--- gear3s-project/src/main/java/cybersoft/java20/dev3lopers/gear3sproject/repository/UserRepository.java
+++ gear3s-project/src/main/java/cybersoft/java20/dev3lopers/gear3sproject/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package cybersoft.java20.dev3lopers.gear3sproject.repository;
 
-import cybersoft.java20.dev3lopers.gear3sproject.entity.Roles;
 import cybersoft.java20.dev3lopers.gear3sproject.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
